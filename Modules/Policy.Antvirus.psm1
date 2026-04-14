1167b70c12dfc4282036d816f6c450db9c656a34
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-04-14 14:38:04 -0400

    Update Policy.Antvirus.psm1

--- Modules/Policy.Antvirus.psm1
+++ Modules/Policy.Antvirus.psm1
@@ -1,42 +1,59 @@
-Set-StrictMode -Version Latest
-$ErrorActionPreference = 'Stop'
+. "$PSScriptRoot\Policy.Common.ps1"
 
 function New-MDEAntivirusPolicy {
-    [CmdletBinding()]
-    param()
-
-    try {
-        $settings = @(
-            @{
-                '@odata.type' = '#microsoft.graph.deviceManagementConfigurationSetting'
-                settingInstance = @{
-                    '@odata.type' = '#microsoft.graph.deviceManagementConfigurationChoiceSettingInstance'
-                    settingDefinitionId = 'device_vendor_msft_policy_config_defender_allowarchivescanning'
-                    settingInstanceTemplateReference = @{
-                        settingInstanceTemplateId = '7c5c9cde-f74d-4d11-904f-de4c27f72d89'
+    New-MDESettingsCatalogResultWrapper -PolicyName 'Antivirus' -CreateAction {
+        $body = @{
+            description = 'Microsoft Defender Antivirus baseline'
+            name        = ''
+            platforms   = 'windows10'
+            roleScopeTagIds = @('0')
+            technologies = 'mdm,microsoftSense'
+            settings = @(
+                @{
+                    '@odata.type' = '#microsoft.graph.deviceManagementConfigurationSetting'
+                    settingInstance = @{
+                        '@odata.type' = '#microsoft.graph.deviceManagementConfigurationChoiceSettingInstance'
+                        settingDefinitionId = 'device_vendor_msft_policy_config_defender_allowarchivescanning'
+                        settingInstanceTemplateReference = @{
+                            settingInstanceTemplateId = '7c5c9cde-f74d-4d11-904f-de4c27f72d89'
+                        }
+                        choiceSettingValue = @{
+                            '@odata.type' = '#microsoft.graph.deviceManagementConfigurationChoiceSettingValue'
+                            value = 'device_vendor_msft_policy_config_defender_allowarchivescanning_1'
+                            children = @()
+                            settingValueTemplateReference = @{
+                                settingValueTemplateId = '9ead75d4-6f30-4bc5-8cc5-ab0f999d79f0'
+                            }
+                        }
                     }
-                    choiceSettingValue = @{
-                        '@odata.type' = '#microsoft.graph.deviceManagementConfigurationChoiceSettingValue'
-                        value = 'device_vendor_msft_policy_config_defender_allowarchivescanning_1'
-                        children = @()
-                        settingValueTemplateReference = @{
-                            settingValueTemplateId = '9ead75d4-6f30-4bc5-8cc5-ab0f999d79f0'
+                },
+                @{
+                    '@odata.type' = '#microsoft.graph.deviceManagementConfigurationSetting'
+                    settingInstance = @{
+                        '@odata.type' = '#microsoft.graph.deviceManagementConfigurationChoiceSettingInstance'
+                        settingDefinitionId = 'device_vendor_msft_policy_config_defender_allowemailscanning'
+                        choiceSettingValue = @{
+                            '@odata.type' = '#microsoft.graph.deviceManagementConfigurationChoiceSettingValue'
+                            value = 'device_vendor_msft_policy_config_defender_allowemailscanning_1'
+                            children = @()
+                        }
+                    }
+                },
+                @{
+                    '@odata.type' = '#microsoft.graph.deviceManagementConfigurationSetting'
+                    settingInstance = @{
+                        '@odata.type' = '#microsoft.graph.deviceManagementConfigurationChoiceSettingInstance'
+                        settingDefinitionId = 'device_vendor_msft_policy_config_defender_realtimescandirection'
+                        choiceSettingValue = @{
+                            '@odata.type' = '#microsoft.graph.deviceManagementConfigurationChoiceSettingValue'
+                            value = 'device_vendor_msft_policy_config_defender_realtimescandirection_0'
+                            children = @()
                         }
                     }
                 }
-            }
-        )
+            )
+        }
 
-        $policy = New-MDEConfigurationPolicy `
-            -PolicyName 'Antivirus' `
-            -Description 'Microsoft Defender Antivirus policy created by deployment tool.' `
-            -Settings $settings
-
-        New-MDELogObject -Name 'Antivirus' -Status 'Success' -Details 'Policy created.' -PolicyId $policy.id
-    }
-    catch {
-        New-MDELogObject -Name 'Antivirus' -Status 'Failed' -Details $_.Exception.Message
+        New-MDEConfigurationPolicy -PolicyName 'Antivirus' -PolicyBody $body
     }
 }
-
-Export-ModuleMember -Function New-MDEAntivirusPolicy
