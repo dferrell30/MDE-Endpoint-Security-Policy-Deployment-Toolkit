d0efbe76bd445c547776d1a9ea75ae771fea46a2
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-04-14 08:46:14 -0400

    Update Bootstrapper.ps1

--- Modules/Bootstrapper.ps1
+++ Modules/Bootstrapper.ps1
@@ -1,1 +1,15 @@
+function Invoke-MDEBootstrap {
 
+    if (-not (Get-Module -ListAvailable Microsoft.Graph)) {
+        Install-Module Microsoft.Graph -Scope CurrentUser -Force
+    }
+
+    Import-Module Microsoft.Graph
+
+    Connect-MgGraph -Scopes @(
+        "DeviceManagementConfiguration.ReadWrite.All",
+        "Directory.Read.All"
+    )
+
+    Select-MgProfile beta
+}
