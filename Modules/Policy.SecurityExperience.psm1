3f780f578b9a205efecfcf5f43ee5d915a079304
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-04-15 20:06:41 -0400

    Update Policy.SecurityExperience.psm1

--- Modules/Policy.SecurityExperience.psm1
+++ Modules/Policy.SecurityExperience.psm1
@@ -1,1 +1,6 @@
+function New-MDESecurityExperiencePolicy {
+    $displayName = Get-DisplayName "Windows Security Experience"
+    return New-MDEPolicyResult -Name $displayName -Status "Info" -Details "Not creating this yet. Leave this for the final pass."
+}
 
+Export-ModuleMember -Function 'New-MDESecurityExperiencePolicy'
