b62bd8bc8508ec7090943199cd121588c3fa51e9
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-04-15 20:05:28 -0400

    Update Policy.EDR.psm1

--- Modules/Policy.EDR.psm1
+++ Modules/Policy.EDR.psm1
@@ -1,1 +1,7 @@
+function New-MDEEDRPolicy {
+    Invoke-CreateTemplatePolicy `
+        -Name "EDR" `
+        -TemplateId "e44c2ca3-2f9a-400a-a113-6cc88efd773d"
+}
 
+Export-ModuleMember -Function 'New-MDEEDRPolicy'
