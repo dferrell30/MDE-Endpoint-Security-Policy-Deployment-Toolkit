410e27a94e57c1b1fd94c248700d861450589c6c
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-05-02 13:08:40 -0400

    Delete Modules/Policy.SecurityExperience.psm1

--- Modules/Policy.SecurityExperience.psm1
+++
@@ -1,6 +0,0 @@
-function New-MDESecurityExperiencePolicy {
-    $displayName = Get-DisplayName "Windows Security Experience"
-    return New-MDEPolicyResult -Name $displayName -Status "Info" -Details "Not creating this yet. Leave this for the final pass."
-}
-
-Export-ModuleMember -Function 'New-MDESecurityExperiencePolicy'
