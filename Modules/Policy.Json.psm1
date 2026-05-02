6f1f6b2e0723834be370132f6102cbc401e8266a
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-05-02 13:13:21 -0400

    Update Policy.Json.psm1

--- Modules/Policy.Json.psm1
+++ Modules/Policy.Json.psm1
@@ -1,7 +1,66 @@
-function New-MDEEDRPolicy {
-    Invoke-CreateTemplatePolicy `
-        -Name "EDR" `
-        -TemplateId "e44c2ca3-2f9a-400a-a113-6cc88efd773d"
+Import-Module "$PSScriptRoot\Common.psm1" -Force -DisableNameChecking
+
+function New-MDEJsonPolicy {
+    param(
+        [Parameter(Mandatory)][string]$Name,
+        [Parameter(Mandatory)][string]$JsonPath,
+        [switch]$WhatIf
+    )
+
+    New-MDEConfigPolicyFromJson `
+        -Name $Name `
+        -JsonPath $JsonPath `
+        -WhatIf:$WhatIf
 }
 
-Export-ModuleMember -Function 'New-MDEEDRPolicy'
+function New-MDEAntivirusSettingsCatalogPolicy {
+    param([switch]$WhatIf)
+
+    $path = Join-Path (Split-Path $PSScriptRoot -Parent) 'Config\SettingsCatalog\antivirus.json'
+    New-MDEJsonPolicy -Name 'Antivirus' -JsonPath $path -WhatIf:$WhatIf
+}
+
+function New-MDEAntivirusEndpointSecurityPolicy {
+    param([switch]$WhatIf)
+
+    $path = Join-Path (Split-Path $PSScriptRoot -Parent) 'Config\EndpointSecurity\antivirus.json'
+    New-MDEJsonPolicy -Name 'Antivirus Endpoint Security' -JsonPath $path -WhatIf:$WhatIf
+}
+
+function New-MDEFirewallPolicy {
+    param([switch]$WhatIf)
+
+    $path = Join-Path (Split-Path $PSScriptRoot -Parent) 'Config\EndpointSecurity\firewall.json'
+    New-MDEJsonPolicy -Name 'Firewall' -JsonPath $path -WhatIf:$WhatIf
+}
+
+function New-MDEASRPolicy {
+    param([switch]$WhatIf)
+
+    $path = Join-Path (Split-Path $PSScriptRoot -Parent) 'Config\EndpointSecurity\asr.json'
+    New-MDEJsonPolicy -Name 'ASR' -JsonPath $path -WhatIf:$WhatIf
+}
+
+function New-MDEEDRPolicy {
+    param([switch]$WhatIf)
+
+    $path = Join-Path (Split-Path $PSScriptRoot -Parent) 'Config\EndpointSecurity\edr.json'
+    New-MDEJsonPolicy -Name 'EDR' -JsonPath $path -WhatIf:$WhatIf
+}
+
+function New-MDEWebProtectionPolicy {
+    param([switch]$WhatIf)
+
+    $path = Join-Path (Split-Path $PSScriptRoot -Parent) 'Config\EndpointSecurity\webprotection.json'
+    New-MDEJsonPolicy -Name 'Web Protection' -JsonPath $path -WhatIf:$WhatIf
+}
+
+Export-ModuleMember -Function @(
+    'New-MDEJsonPolicy',
+    'New-MDEAntivirusSettingsCatalogPolicy',
+    'New-MDEAntivirusEndpointSecurityPolicy',
+    'New-MDEFirewallPolicy',
+    'New-MDEASRPolicy',
+    'New-MDEEDRPolicy',
+    'New-MDEWebProtectionPolicy'
+)
