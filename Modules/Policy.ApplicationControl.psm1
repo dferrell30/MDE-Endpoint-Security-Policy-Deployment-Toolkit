32ab30fadeddb2879124718e5410ce2f417ad831
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-05-02 13:08:15 -0400

    Delete Modules/Policy.ApplicationControl.psm1

--- Modules/Policy.ApplicationControl.psm1
+++
@@ -1,7 +0,0 @@
-function New-MDEApplicationControlPolicy {
-    Invoke-CreateTemplatePolicy `
-        -Name "Application Control" `
-        -TemplateId "63be6324-e3c9-4c97-948a-e7f4b96f0f20"
-}
-
-Export-ModuleMember -Function 'New-MDEApplicationControlPolicy'
